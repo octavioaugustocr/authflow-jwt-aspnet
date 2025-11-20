4154d89b21de495ff646a4d1320d0b50e3f7e061
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-19 22:15:20 -0300

    criando páginas e alterando alguns estilos

---
+++ AuthFlow/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthFlow.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ AuthFlow/Controllers/RegisterController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthFlow.Controllers;
+
+public class RegisterController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
