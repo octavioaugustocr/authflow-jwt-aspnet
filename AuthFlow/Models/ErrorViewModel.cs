4c0137208d9fefb97c7ab956038338c222d9c53c
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-19 21:28:30 -0300

    primeiro commit: inicializando o projeto

---
+++ AuthFlow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AuthFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
