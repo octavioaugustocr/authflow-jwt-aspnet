80c3dceb3e64dbc7f23999c1936606d4ea8a0cf2
Octávio Augusto Campos Ribeiro <octavioaugusto5566@gmail.com>
2025-11-28 22:26:19 -0300

    criando o AppDbContext e configurando ele

---
+++ AuthFlow/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using AuthFlow.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthFlow.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<UserModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserModel>(entity => entity.HasKey(u => u.IdUser));
+
+            modelBuilder.Entity<UserModel>
+        }
+    }
+}
